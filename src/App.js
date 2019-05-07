04c01a9853e39640ae67d4dfaf76942861a19278
ExpPool <candra.f@mail.ugm.ac.id>
2019-05-07 13:38:00 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import './App.css';
+import {Link} from 'react-router-dom';
+import Main from "./component/Main";
+
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <Link to="/home">Home</Link>&nbsp;&nbsp;
+                <Link to="/profile">Profile</Link>&nbsp;&nbsp;
+                <Link to="/portofolio">Portofolio</Link>&nbsp;&nbsp;
+                <Link to="/kontak">Kontak</Link>&nbsp;&nbsp;
+                <p><Main/></p>
+            </div>
+        );
+    }
+}
+
+export default App;
